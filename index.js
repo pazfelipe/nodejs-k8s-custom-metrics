13d865dc6db47145f0a6b8c179be7b58b7343b24
Felipe Paz <xxfelipepaz@gmail.com>
2022-10-03 10:03:03 -0300

    feat: initial app

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const { Counter, register } = require('prom-client');
+
+
+const app = express();
+
+app.use(express.json());
+
+const counter = new Counter({
+  name: 'my_counter',
+  help: 'Example of a counter',
+  labelNames: ['method', 'path']
+});
+
+const fibonacci = (n) => n < 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+
+app.post('/', (req, res) => {
+  counter.inc({ method: req.method, path: req.path });
+  res.send(`Fibonacci of ${req.body.number} is ${fibonacci(req.body.number)}`);
+});
+
+app.get('/metrics', async (req, res) => {
+  res.header('Content-Type', 'application/json');
+  res.send(await register.getSingleMetricAsString('my_counter'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
